6bb84ddea814bcca21012903500f4e57442577a0
Baizid Bostami <mdbaizidbostami196@gmail.com>
2026-02-28 23:40:20 +0600

    Hero section done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,88 @@
+import designImage from "../assets/Group.png";
+import heroImaeg from "../assets/Hero_Right_image/Hero_image.png";
+import heroPatten from "../assets/Hero_Right_image/Pattern.png";
+import rectangleImage from "../assets/Rectangle2732.png";
+import { FiSearch, FiMapPin } from "react-icons/fi";
 
 const Home = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="py-16 relative">
+      {/* Background Graphic placeholder */}
+      <div className="absolute -right-10 top-90 h-62.5  md:-right-30 md:-top-16.5 md:w-[90%]">
+        <img src={heroPatten} alt="Hero Pattern" />
+      </div>
+
+      <div className="flex flex-col lg:flex-row items-top justify-between gap-12 relative ">
+        {/* Left Content */}
+        <div className="flex-1 space-y-10">
+          <h1 className="text-5xl clashFont lg:text-7xl font-bold text-[#1F2937] leading-[1.1]">
+            Discover <br />
+            more than <br />
+            <span className="text-[#00B4FF] relative inline-block">
+              5000+ Jobs
+              {/* Replace with actual squiggly line image */}
+              <img
+                className="w-112.5"
+                src={designImage}
+                alt="Hero Section design"
+              />
+            </span>
+          </h1>
+          <p className="text-gray-500 text-lg max-w-md leading-relaxed">
+            Great platform for the job seeker that searching for new career
+            heights and passionate about startups.
+          </p>
+
+          {/* Search Box */}
+          <div className="w-full md:w-200 absolute z-90 h-22.25 bg-white p-4 shadow-[0_8px_30px_rgb(0,0,0,0.08)] flex flex-col md:flex-row items-center gap-2 ">
+            <div className="flex items-center gap-2 flex-1 px-4 py-2 w-full md:w-auto md:border-r border-gray-200">
+              <FiSearch className="text-[#25324B] text-2xl" />
+              <input
+                type="text"
+                placeholder="Job title or keyword"
+                className="bg-transparent py-2.25 outline-none w-full text-gray-700 placeholder-gray-400 border-b-[1px] border-[#7C8493]"
+              />
+            </div>
+            <div className="flex items-center gap-2 flex-1 px-4 py-2 w-full md:w-auto">
+              <FiMapPin className="text-[#25324B] text-2xl" />
+              <select defaultValue="Pick a color" className=" focus:outline-none bg-transparent py-2.25 w-full text-gray-700 placeholder-gray-400 border-b-[1px] border-[#7C8493]">
+                <option disabled={false}>Select location</option>
+                <option>Florence, Italy</option>
+                <option>New York, USA</option>
+                <option>London, UK</option>
+              </select>
+            </div>
+
+            <button className="bg-[#4C3EE8] hover:bg-[#3b2fc9] text-white px-8 py-3 w-full md:w-auto font-medium transition-colors">
+              Search my job
+            </button>
+          </div>
+
+          <p className="text-sm absolute bottom-28 text-gray-500">
+            Popular :{" "}
+            <span className="font-medium text-gray-700">
+              UI Designer, UX Researcher, Android, Admin
+            </span>
+          </p>
         </div>
-    );
+
+        {/* Right Image */}
+        <div className="flex-1 hidden md:flex justify-center lg:justify-end relative">
+          {/* Use your provided man pointing image here */}
+          <img
+            src={heroImaeg}
+            alt="Happy Man Pointing"
+            className="shape w-full max-w-125 object-cover rounded-full lg:rounded-none z-10"
+          />
+          <img
+            src={rectangleImage}
+            alt="Rectangle Image"
+            className="absolute -bottom-20 w-[90%] z-10"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,37 +4,15 @@ import HambarIcon from "../assets/Mobile/Vector.svg";
 
 const NavBar = () => {
   const link = [
-    <li key="1">
-      <NavLink to="/" className="">
-        Find Jobs
-      </NavLink>
-    </li>,
-    <li key="2">
-      <NavLink to="/about" className="">
-        Browse Companies
-      </NavLink>
-    </li>,
-    <li key="3">
-      <NavLink
-        to="/Login"
-        className="md:hidden btn btn-ghost text-blue-600 font-semibold hover:bg-blue-50"
-      >
-        Login
-      </NavLink>
-    </li>,
-    <li key="4">
-      <NavLink
-        to="/signup"
-        className="md:hidden btn bg-[#4C3EE8] hover:bg-[#3b2fc9] text-white border-none px-6"
-      >
-        signup
-      </NavLink>
-    </li>,
+    <li key="1" className="h-3.5"><NavLink to="/" className="">Find Jobs</NavLink></li>,
+    <li key="2" className="h-3.5"><NavLink to="/about" className="">Browse Companies</NavLink></li>,
+    <li key="3"><NavLink to="/Login"className="md:hidden btn btn-ghost text-blue-600 font-semibold hover:bg-blue-50">Login</NavLink></li>,
+    <li key="4"><NavLink to="/signup"className="md:hidden btn bg-[#4C3EE8] hover:bg-[#3b2fc9] text-white border-none px-6">signup</NavLink></li>,
   ];
 
   return (
-    <div className="navbar bg-#D6DDEB md:bg-transparent">
-      <div className="navbar-start">
+    <div className="navbar md:bg-transparent">
+      <div className="navbar-start  gap-5">
         <Link to="/" className="">
           <img
             src={logoImage}
@@ -43,8 +21,8 @@ const NavBar = () => {
           />
         </Link>
 
-        <div className="navbar-start hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{link}</ul>
+        <div className="hidden lg:flex">
+          <ul className="flex flex-row padding-[0px] gap-5">{link}</ul>
         </div>
       </div>
 
@@ -63,13 +41,13 @@ const NavBar = () => {
           </Link>
         </div>
 
-        <div className="dropdown dropdown-bottom dropdown-end ">
-          <div tabIndex={0} role="button" className="btn btn-ghost bg- lg:hidden">
+        <div className="dropdown dropdown-bottom dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost bg- lg:hidden bg-white">
             <img src={HambarIcon} alt="Hamburger menu icon" className="h-5 w-5" />
           </div>
           <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-white rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             {link}
           </ul>
